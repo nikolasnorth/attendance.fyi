3afcdf24ebf4d9bb88350866fcfaf884c3026f61
Nikolas North <nikolasn09@gmail.com>
2022-05-15 13:49:29 -0400

    Create logo component for homepage banner

---
+++ frontend/components/Logo.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+  return (
+    <div className="-rotate-6 bg-green-500 p-2 text-3xl font-bold">
+      <span>attendance.fyi</span>
+    </div>
+  )
+}
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,11 @@
 import Banner from "../components/Banner";
+import Logo from "../components/Logo";
 
 export default function Home() {
   return (
-    <Banner/>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Logo/>
+      <Banner/>
+    </div>
   );
 }
